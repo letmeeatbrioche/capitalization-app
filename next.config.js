fa6864d1c31a3a348c53d2fececc58823ead55cb
Haillie W <hailliewilloughby@gmail.com>
2023-09-28 20:31:53 -0500

    Add package files and Next.js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  transpilePackages: ['@acme/ui', 'lodash-es'],
+}
