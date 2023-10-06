a6811417fa0a0dea476920c4fb5cd3c615b74ddb
Haillie W <hailliewilloughby@gmail.com>
2023-10-05 19:25:55 -0500

    Import/export option comps

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+import Input from "./Input";
+import AllCapsOption from "./AllCapsOption";
+import AllLowercaseOption from "./AllLowercaseOption";
+import CapEveryOtherLetterOption from "./CapEveryOtherLetterOption";
+import CapEveryWordOption from "./CapEveryWordOption";
+import SentenceCapsOption from "./SentenceCapsOption";
+
+export {
+  Input,
+  AllCapsOption,
+  AllLowercaseOption,
+  CapEveryOtherLetterOption,
+  CapEveryWordOption,
+  SentenceCapsOption,
+}
