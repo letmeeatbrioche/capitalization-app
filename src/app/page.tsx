006e283b8bf4b34c1cd644003be85f27a46272df
Haillie W <hailliewilloughby@gmail.com>
2023-10-05 20:19:23 -0500

    Add Result comp to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Input } from "../components";
+import { Input, Result } from "../components";
 
 export default function Home() {
   return (
     <div>
       Home
       <Input />
+      <Result />
     </div>
   )
 }
