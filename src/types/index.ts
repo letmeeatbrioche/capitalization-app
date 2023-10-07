2e9f2778a204ad1bb9c8d94228d304e5613c81a9
Haillie W <hailliewilloughby@gmail.com>
2023-10-07 17:21:31 -0500

    Add interfaces for comps with props

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+import { MouseEventHandler } from "react"
+
+export interface AllCapsOptionProps {
+  selectOption: MouseEventHandler<HTMLButtonElement>
+}
+
+export interface AllLowercaseOptionProps {
+  selectOption: MouseEventHandler<HTMLButtonElement>
+}
+
+export interface CapEveryOtherLetterOptionProps {
+  selectOption: MouseEventHandler<HTMLButtonElement>
+}
+
+export interface CapEveryWordOptionProps {
+  selectOption: MouseEventHandler<HTMLButtonElement>
+}
+
+export interface SentenceCapsOptionProps {
+  selectOption: MouseEventHandler<HTMLButtonElement>
+}
+
+export interface TransformedTextProps {
+  transformedText: string
+}
