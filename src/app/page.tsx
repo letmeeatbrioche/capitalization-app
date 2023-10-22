310f59d02bd1576fa3c0ced7ca15c3c0e01ed8d6
Haillie W <hailliewilloughby@gmail.com>
2023-10-22 13:29:02 -0500

    Clean up page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+"use client"
 import { Form } from "../components";
+import { useState } from "react";
 
 export default function Home() {
+  const [selected, setSelected] = useState('');
   return (
     <div>
       <Form />
+      <h3>{selected}</h3>
     </div>
   )
 }
+
+
+
