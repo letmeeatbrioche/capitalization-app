ea6788151f6f44792038b96c1e1480c20ef01250
Haillie W <hailliewilloughby@gmail.com>
2023-11-07 15:28:58 -0600

    Add result box styles

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -2,7 +2,7 @@ import { TransformedTextProps } from "@/types"
 
 const Result = ({ transformedText }: TransformedTextProps) => {
   return (
-    <div className="result bg-[rgb(242, 248, 255)] border-2 border-black border-solid rounded-[23px] col-[5_/_span_3] w-[105%] relative right-[42px] p-2.5 max-h-[423.2px]">
+    <div className="result overflow-auto text-[18px] bg-[#f2f8ff] border-2 border-black border-solid rounded-[23px] col-[5_/_span_3] w-[105%] relative right-[42px] p-2.5 max-h-[423.2px]">
       {transformedText}
     </div>
   )
