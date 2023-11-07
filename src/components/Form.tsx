cf2b5dd9698bbef12d2ec5a728d4f59355b66ea9
Haillie W <hailliewilloughby@gmail.com>
2023-11-07 15:28:35 -0600

    Add form styles

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -59,25 +59,25 @@ const Form = () => {
   }
 
   return (
-    <div className="container z-50 bg-[rgb(213, 227, 241)] border-2 border-black border-solid rounded-[20px] max-w-[980px] my-[40px] mx-auto h-[90vh] p-2.5">
-      <form className="form bg-[rgb(213, 227, 241)] border-2 border-black border-solid grid grid-cols-7 grid-rows-[3fr_3%_1fr_1fr] h-full w-full" onSubmit={handleSubmit}>
-        <textarea className="user-input bg-[rgb(242, 248, 255)] border-2 border-black border-solid rounded-[23px] col-[1_/_span_3] w-[105%] p-2.5" name="userText" value={text} onChange={(event) => handleInputChange(event)} />
+    <div className="container shadow-container-shadow z-50 bg-[#d5e3f1] rounded-[20px] max-w-[980px] my-[40px] mx-auto h-[90vh] p-2.5">
+      <form className="form bg-[#d5e3f1] grid grid-cols-7 grid-rows-[3fr_3%_1fr_1fr] h-full w-full" onSubmit={handleSubmit}>
+        <textarea className="user-input font-arial-sans text-[18px] bg-[rgb(242, 248, 255)] border-2 border-black border-solid rounded-[23px] col-[1_/_span_3] w-[105%] p-2.5" name="userText" value={text} onChange={(event) => handleInputChange(event)} />
 
         <Result transformedText={transformed} />
 
-        <div className="options border-2 border-black border-solid flex w-full col-[1_/_span_7] row-start-3">
+        <div className="options flex w-full col-[1_/_span_7] row-start-3">
           <input className="option hidden" type="radio" name="option" value="All caps" id="all-caps" onChange={() => handleOptionClick()}/>
-          <label htmlFor="all-caps" className="bg-[#1d2d] h-8 w-[200px] text-[17px] border-2 border-black border-solid rounded-[2rem] text-center justify-self-center" >ALL CAPS</label>
+          <label htmlFor="all-caps" className="cursor-pointer bg-[#deefff] hover:bg-[#e2ecf6] h-8 w-[200px] p-label-padding text-[17px] border-2 border-black border-solid rounded-[2rem] text-center justify-self-center" >ALL CAPS</label>
 
 
           <input className="option hidden" type="radio" name="option" value="All lowercase" id="lowercase" onChange={() => handleOptionClick()}/>
-          <label className="down-btn self-end h-8 w-[200px] text-[17px] border-2 border-black border-solid rounded-[2rem] text-center justify-self-center" htmlFor="lowercase">lowercase</label>
+          <label className="down-btn self-end cursor-pointer bg-[#deefff] hover:bg-[#e2ecf6] h-8 w-[200px] p-label-padding text-[17px] border-2 border-black border-solid rounded-[2rem] text-center justify-self-center" htmlFor="lowercase">lowercase</label>
 
           <div className="special-option top-[50px] grid col-[4_/_span_1] justify-self-center">
               <input className="option every-other-letter hidden" type="radio" name="option" value="Every other letter" id="every-other-letter" onChange={() => handleOptionClick('special')} />
-              <label htmlFor="every-other-letter" className="h-8 w-[200px] text-[17px] border-2 border-black border-solid rounded-[2rem] text-center justify-self-center order-1">Every other letter</label>
+              <label htmlFor="every-other-letter" className="cursor-pointer bg-[#deefff] hover:bg-[#e2ecf6] h-8 w-[200px] p-label-padding text-[17px] border-2 border-black border-solid rounded-[2rem] text-center justify-self-center order-1">Every other letter</label>
 
-              <label className="checkbox-label relative -top-28 h-8 w-[200px] text-[17px] border-2 border-black border-solid rounded-[2rem] text-center justify-self-center order-2" htmlFor="checkbox" style={{cursor: everyOtherLetterSelected ? 'pointer' : undefined}}>
+              <label className="checkbox-label relative -top-[25px] h-8  p-checkbox-padding text-[17px] rounded-[2rem] text-center justify-self-center order-2" htmlFor="checkbox" style={{cursor: everyOtherLetterSelected ? 'pointer' : undefined, backgroundColor: checkboxActive ? '#c7d3ff' : 'transparent', border: checkboxActive ? 'solid 2px black': 'none'}}>
                 <input className="checkbox" type="checkbox" name="first" id="checkbox" disabled={everyOtherLetterSelected ? false : true}  checked={checkboxActive} onClick={handleCheckboxClick} />
                 Cap. the FIRST letter
               </label>
@@ -86,14 +86,14 @@ const Form = () => {
 
 
           <input className="option hidden" type="radio" name="option" value="Every word" id="every-word" onChange={() => handleOptionClick()}/>
-          <label className="down-btn self-end h-8 w-[200px] text-[17px] border-2 border-black border-solid rounded-[2rem] text-center justify-self-center" htmlFor="every-word">Every Word</label>
+          <label className="down-btn self-end cursor-pointer bg-[#deefff] hover:bg-[#e2ecf6] h-8 w-[200px] p-label-padding text-[17px] border-2 border-black border-solid rounded-[2rem] text-center justify-self-center" htmlFor="every-word">Every Word</label>
 
 
           <input className="option hidden" type="radio" name="option" value="Sentence caps" id="sentence" onChange={() => handleOptionClick()}/>
-          <label htmlFor="sentence" className="h-8 w-[200px] text-[17px] border-2 border-black border-solid rounded-[2rem] text-center justify-self-center">Sentence caps</label>
+          <label htmlFor="sentence" className="cursor-pointer bg-[#deefff] hover:bg-[#e2ecf6] h-8 w-[200px] p-label-padding text-[17px] border-2 border-black border-solid rounded-[2rem] text-center justify-self-center">Sentence caps</label>
         </div>
 
-        <button className="transform border-2 border-black border-solid rounded-[96px] col-[4_/_span_1] w-[300px] h-[75px] row-start-4 self-center text-[17.5px] uppercase justify-self-center" type="submit" disabled={selected ? false : true} style={!selected ? disabledTransformStyles : undefined}>Transform</button>
+        <button className="transform font-arial-sans bg-[#2b73b7] border-2 border-black border-solid rounded-[96px] col-[4_/_span_1] w-[300px] h-[75px] row-start-4 self-center text-[17.5px] text-white font-semibold uppercase justify-self-center" type="submit" disabled={selected ? false : true} style={!selected ? disabledTransformStyles : undefined}>Transform</button>
 
         {/* <AllCapsOption selectOption={() => handleOptionClick('All caps')} /> */}
         {/* <AllLowercaseOption selectOption={() => handleOptionClick('All lowercase')} /> */}
