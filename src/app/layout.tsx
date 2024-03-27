c304910be0493d3d92a2a568ecc94acba752f65a
Haillie W <hailliewilloughby@gmail.com>
2024-03-27 14:44:29 -0500

    Change metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'CapApp',
+  title: 'CAPi',
   description: 'Capitalize your text in many different ways',
 }
 
