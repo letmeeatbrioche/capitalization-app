4acbecc2c545d3997e78c10457297a6ea0fbf39b
Haillie W <hailliewilloughby@gmail.com>
2024-03-27 14:44:44 -0500

    Add responsive styling

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -2,7 +2,7 @@ import { TransformedTextProps } from "@/types"
 
 const Result = ({ transformedText }: TransformedTextProps) => {
   return (
-    <div className="result overflow-auto text-[18px] bg-[#f2f8ff] border-2 border-black border-solid rounded-[23px] col-[5_/_span_3] w-[105%] relative right-[42px] p-2.5 max-h-[423.2px]">
+    <div className="result overflow-auto text-[18px] bg-[#f2f8ff] border-2 border-black border-solid rounded-[23px] p-2.5 min-h-[90px] h-auto max-h-[423.2px]">
       {transformedText}
     </div>
   )
