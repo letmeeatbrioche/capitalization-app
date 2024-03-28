218e41325763bd5b2641bf5476c49f9e55080705
Haillie W <hailliewilloughby@gmail.com>
2024-03-27 19:34:36 -0500

    Add placeholder text to result box

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -2,8 +2,11 @@ import { TransformedTextProps } from "@/types"
 
 const Result = ({ transformedText }: TransformedTextProps) => {
   return (
-    <div className="result overflow-auto text-[18px] bg-[#f2f8ff] border-2 border-black border-solid rounded-[23px] p-2.5 min-h-[90px] h-auto max-h-[423.2px]">
-      {transformedText}
+    <div className="result overflow-auto text-[18px] bg-[#ffffff] border-2 border-black border-solid rounded-[23px] p-2.5 min-h-[100px] h-auto max-h-[423.2px]">
+      {
+        transformedText ||
+        <p className="result text-stone-400">Result</p>
+      }
     </div>
   )
 }
